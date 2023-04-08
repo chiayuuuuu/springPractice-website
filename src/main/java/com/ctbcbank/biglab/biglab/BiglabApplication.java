8aefa1893220c7a90b8906e0ed58f0968dfeace7
Carrie <carriechang0419@gmail.com>
2023-04-08 17:02:24 +0800

    新增專案

---
+++ src/main/java/com/ctbcbank/biglab/biglab/BiglabApplication.java
@@ -0,0 +1,13 @@
+package com.ctbcbank.biglab.biglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiglabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiglabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctbcbank/biglab/biglab/BiglabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctbcbank.biglab.biglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiglabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
